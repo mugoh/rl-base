41489c9586d160285cd8a5f1255f9037eaa2e094
mugoh <mugoh.ks@gmail.com>
2020-11-22 23:23:23 +0300

    d2rl: add critic network

--- d2rl/d2rl.py
+++ d2rl/d2rl.py
@@ -3,6 +3,7 @@
 """
 from typing import Optional
 
+import torch
 import torch.nn as nn
 
 
@@ -45,10 +46,73 @@ class PolicyMLP(nn.Module):
         """
             Predict action for given observation
         """
-        for layer in self.pi[-1]:
+        # Don't concat last two layers
+        # Remeber each layer has an activation function
+        # So count = 4
+        for layer in self.pi[:-4]:
             x = layer(obs)
-            obs = torch.cat([x, obs], dim=-1)
+            x = torch.cat([x, obs], dim=-1)
 
-        act = self.pi[-1](obs)
+        for layer in self.pi[:-4]:
+            x = layer(x)
+
+        act = x
+        return act
+
+
+class MLPQFunction(nn.Module):
+    """
+        Q Function (D2RL)
+
+        The action-value funtion
+    """
+
+    def __init__(self, obs_dim: int, act_dim: int, *,
+                 hidden_sizes: list, activation: Optional[object] = nn.ReLU,
+                 output_activation: Optional[object] = nn.Identity,
+                 size: Optional[int] = 4):
+        super(MLPQFunction, self).__init__()
+
+        self.q = mlp(act_dim + obs_dim, hidden_sizes + [1],
+                     activation, size, output_activation)
+
+    def forward(self, obs, act):
+        inpt = torch.cat([obs, act], dim=-1)
+
+        for layer in self.q[:-4]:
+            x = layer(inpt)
+            x = torch.cat([x, inpt], dim=-1)
+
+        # No concat for last 2 layers
+        for layer in self.q[-4:]:
+            x = layer(x)
+
+        out = x.unsqueeze()
+
+        return out
+
+
+class MLPActorCritic(nn.module):
+    def __init__(self, obs_dim: int, act_dim: int,
+                 hidden_sizes: Optional[list] = [64] * 5,
+                 activation: Optional[object] = nn.ReLU,
+                 output_activation: Optional[object] = nn.Identity,
+                 size: Optional[int] = 5):
+        self.q = MLPQFunction(obs_dim, act_dim,
+                              hidden_sizes=hidden_sizes,
+                              activation=activation,
+                              size=size)
+        self.pi = PolicyMLP(obs_dim, act_dim,
+                            hidden_sizes=hidden_sizes,
+                            activation=activation,
+                            output_activation=nn.Tanh,
+                            size=size)
+
+    def act(self, obs):
+        """
+            Predict action for observation
+        """
+        with torch.no_grad():
+            act = self.pi(obs)
 
         return act
